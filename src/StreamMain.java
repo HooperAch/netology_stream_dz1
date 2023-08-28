b9b2c18e90bc7ca9fcdafdca04eaa4faf617c038
filyuzin <forgogu@inbox.ru>
2023-08-28 23:04:42 +0700

    Fixed remark

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -9,12 +9,11 @@ public class StreamMain {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
         //через Stream API
-        Stream<Integer> stream = intList.stream();
-        stream
+        Stream<Integer> stream = intList.stream()
                 .filter(x -> x > 0)
                 .filter(x -> x % 2 == 0)
-                .sorted(Comparator.naturalOrder())
-                .forEach(System.out::println);
-
+                .sorted(Comparator.naturalOrder());
+                //.forEach(System.out::print); // ERROR: java: incompatible types: void cannot be converted to java.util.stream.Stream<java.lang.Integer>
+        stream.forEach(System.out::println);
     }
 }
